257d9d6aa814c2f2f534f1b653455c53cb8acefa
João Victor dos Santos Mendes <jvictorsantos852@gmail.com>
2023-06-20 12:10:33 -0300

    feat: added testing config

---
+++ vitest.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react-swc';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    files: ['./src/**/*.test.tsx'],
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    // you might want to disable it, if you don't have tests that rely on CSS
+    // since parsing CSS is slow
+    css: true,
+    passWithNoTests: true,
+  },
+});
