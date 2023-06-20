a52db7eaa22016d60e26f316cfd05ec9d85474eb
João Victor dos Santos Mendes <jvictorsantos852@gmail.com>
2023-06-20 12:40:33 -0300

    feat: added router

---
+++ src/router/routes.tsx
@@ -0,0 +1,15 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from '../pages/Home';
+
+const Routes = () => {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+};
+
+export default Routes;
