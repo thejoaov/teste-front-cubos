6cae9b6ed938cde54f2410cfb8d9838c9d14a80f
João Victor dos Santos Mendes <jvictorsantos852@gmail.com>
2023-06-21 22:24:53 -0300

    feat: color theme definition

---
+++ src/utils/colors.ts
@@ -0,0 +1,9 @@
+export const colors = {
+  primary: '#116193',
+  secondary: '#06dfdf',
+  background: '#ebebeb',
+  placeholder: '#a4becf',
+  text: '#686868',
+  textInverted: '#ffffff',
+  textLight: '#ababab',
+};
