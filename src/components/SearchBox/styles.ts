ef67d0ec60365c6c96ee1067df58e0c3c8982f01
João Victor dos Santos Mendes <jvictorsantos852@gmail.com>
2023-06-21 22:25:16 -0300

    refactor: applying color theme definition to components

--- src/components/SearchBox/styles.ts
+++ src/components/SearchBox/styles.ts
@@ -1,16 +1,17 @@
 import styled from 'styled-components';
+import { colors } from '../../utils/colors';
 
 export const StyledSearchBox = styled.input`
   width: 100%;
   height: 50px;
   border-radius: 25px;
   border: none;
-  background-color: #ebebeb;
+  background-color: ${colors.background};
   font-family: 'Abel', sans-serif;
   padding: 0 5vw 0 5vw;
   font-size: 18px;
 
   &::placeholder {
-    color: #a4becf;
+    color: ${colors.placeholder};
   }
 `;
--- src/components/TopBar/styles.ts
+++ src/components/TopBar/styles.ts
@@ -1,16 +1,17 @@
 import styled from 'styled-components';
+import { colors } from '../../utils/colors';
 
 export const TopBarContainer = styled.div<{ testID?: string }>`
   flex: 1;
   display: flex;
   height: 80px;
-  background-color: #116193;
+  background-color: ${colors.primary};
   justify-content: center;
   align-items: center;
 
   h1 {
     font-family: 'Abel', sans-serif;
     font-size: 50px;
-    color: #06dfdf;
+    color: ${colors.secondary};
   }
 `;
