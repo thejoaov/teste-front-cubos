da29a0cbc579c5749469d34057baa3f86a3e6267
João Victor dos Santos Mendes <jvictorsantos852@gmail.com>
2023-06-22 10:18:49 -0300

    feat: preload languages on cached info context

--- src/contexts/cachedInfo/index.tsx
+++ src/contexts/cachedInfo/index.tsx
@@ -5,12 +5,14 @@ import React, {
   useEffect,
   useState,
 } from 'react';
-import { Genre } from '../../services/api/types';
-import { getGenres } from '../../services/api';
+import { Genre, Language } from '../../services/api/types';
+import { getGenres, getLanguages } from '../../services/api';
 
 export type CachedInfo = {
   genres: Genre[] | null;
   translateGenres: (ids: number[]) => string[];
+  languages: Language[] | null;
+  getLanguageByName: (name: string) => string;
 };
 
 export type CachedInfoData = CachedInfo;
@@ -23,13 +25,23 @@ export const CachedInfoProvider: React.FC<PropsWithChildren> = ({
   children,
 }) => {
   const [genres, setGenres] = useState<Genre[] | null>(null);
+  const [languages, setLanguages] = useState<Language[] | null>(null);
+
+  const handleGetLanguages = async () => {
+    try {
+      const response = await getLanguages();
+      setLanguages(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const handleGetGenres = async () => {
     try {
       const response = await getGenres();
       setGenres(response.data.genres);
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
   };
 
@@ -43,15 +55,27 @@ export const CachedInfoProvider: React.FC<PropsWithChildren> = ({
     [genres]
   );
 
+  const getLanguageByName = useCallback(
+    (name: string) => {
+      if (!languages) return '';
+      const language = languages.find((item) => item.iso_639_1 === name);
+      return language ? language.name : '';
+    },
+    [languages]
+  );
+
   useEffect(() => {
+    handleGetLanguages();
     handleGetGenres();
   }, []);
 
   return (
     <CachedInfoContext.Provider
       value={{
+        languages,
         genres,
         translateGenres,
+        getLanguageByName,
       }}
     >
       {children}
